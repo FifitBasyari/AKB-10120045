72864b07893fea0bd85655c2431500ebe2b0de9b
fifitbasyari <fifitbasyari12@gmail.com>
2023-08-12 19:34:55 +0700

    first commit

---
+++ app/src/main/java/com/uas/notes/Config.java
@@ -0,0 +1,15 @@
+/**
+ * NIM : 10120045
+ * NAMA : Fifit Siti Nurfitriyati Basyari
+ * KELAS : IF-2
+ */
+package com.uas.notes;
+
+public class Config {
+
+    public static String DB_URL = "https://uas-10120045-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+    public static String getDB_URL() {
+        return DB_URL;
+    }
+}
---
+++ app/src/main/java/com/uas/notes/model/NoteCategory.java
@@ -0,0 +1,10 @@
+/**
+ * NIM : 10120045
+ * NAMA : Fifit Siti Nurfitriyati Basyari
+ * KELAS : IF-2
+ */
+package com.uas.notes.model;
+
+public class NoteCategory {
+    public String title, total;
+}
